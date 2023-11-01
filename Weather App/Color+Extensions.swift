0a53510fe7c499a65218c4b83266d5e861e14824
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-11-01 16:33:48 -0300

    created screen

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Gabriel Jesus on 01/11/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "primaryColor")
+    static let gray = UIColor(named: "gray")
+}
